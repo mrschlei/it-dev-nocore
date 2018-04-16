8b0f427fc3d1b97ce9b1c0ba6d770fc06c07b578
mrschlei <mrschlei@umich.edu>
2018-04-16 15:51:16 -0400

    copy-paste, baby

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Redirect logs to stdout and stderr for docker reasons.
+# it seems both sylinks already exist. 
+# these commands create unnecessary duplicates
 ln -sf /dev/stdout /var/log/apache2/access_log
 ln -sf /dev/stderr /var/log/apache2/error_log
 
 # apache and virtual host secrets
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
 ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
-#ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
-#ln -sf /secrets/apache2/ports.conf /etc/apache2/mods-available/ports.conf
+ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
 
 # SSL secrets
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
@@ -20,16 +21,15 @@ then
   /bin/sh /secrets/app/local.start.sh
 fi
 
-### Rehash command needs to be run before starting apache.
-c_rehash /etc/ssl/certs /secrets/ssl
+## Rehash command needs to be run before starting apache.
+c_rehash /etc/ssl/certs >/dev/null
 
-a2enmod cosign
 a2enmod ssl
 a2enmod include
 a2ensite default-ssl 
 
 #cd /var/www/html
-##drush @sites cc all --yes
-##drush up --no-backup --yes
+#drush @sites cc all --yes
+#drush up --no-backup --yes
 
-/bin/sh -x /usr/local/bin/apache2-foreground
+/usr/local/bin/apache2-foreground
